0e420cc3e5c02d90e249c566c36b514cdb4511f9
manuelgon47 <manuelgon47@gmail.com>
2018-01-16 21:29:55 +0100

    Update sed command

--- step.sh
+++ step.sh
@@ -4,7 +4,6 @@ set -e
 #
 # Required parameters
 pwd = `pwd`
-echo $pwd
 if [ -z "${gradle_file_path}" ] ; then
   echo " [!] Missing required input: gradle_file_path"
   exit 1
@@ -15,14 +14,12 @@ if [ ! -f "${gradle_file_path}" ] ; then
 fi
 
 old_version_name=`grep -m 1 versionName ${gradle_file_path} | sed 's/^[[:blank:]]*versionName[[:blank:]]*[[:blank:]]*\"\([^\"]*\)\".*$/\1/'`
-
 if [ -z "${old_version_name}" ] ; then
   echo " [!] No versionName founded on $gradle_file_path"
   exit 1
 fi
 
 old_version_code=`grep versionCode ${gradle_file_path} | sed 's/.*versionCode*"//;s/".*//' | awk '{print $2}'`
-
 if [ -z "${old_version_code}" ] ; then
   echo " [!] No versionCode founded on $gradle_file_path"
   exit 1
@@ -31,7 +28,7 @@ fi
 # Set version code
 if [ -n "${version_code}" ] ; then
 	new_version_code=$(($version_code+$version_code_offset))
-	sed -i "versionCode $old_version_code/versionCode $new_version_code/s"
+	sed -i.bak "s/versionCode $old_version_code/versionCode $new_version_code/g" $gradle_file_path
 	version_code=new_version_code
 else
 	version_code=$old_version_code
@@ -39,7 +36,7 @@ fi
 
 # Set version name
 if [ -n "${version_name}" ] ; then
-	sed -i "versionName $old_version_name/versionName $version_name/s"
+	sed -i.bak "versionName $old_version_name/versionName $version_name/s" $gradle_file_path
 else
 	version_name=$old_version_name
 fi
